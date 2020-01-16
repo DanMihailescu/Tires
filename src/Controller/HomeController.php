ad70c21ab3c6e6f8ff1a2af9fb2f52e60734e868
DanMihailescu <as42554525@yahoo.ca>
2020-01-16 14:34:38 -0500

    Initial Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    public function home()
+    {
+    	include 'home.html';
+        return new Response(
+        );
+    }
+}
+?>
